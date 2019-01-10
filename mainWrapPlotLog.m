5edbbdd3b583263f5153c20b87eb8f3a220c96e6
moucrob <mathiasmouchrik@gmail.com>
2019-01-10 21:02:35 +0000

    ok so in the commit 47944614758ab the ticks were healthy, noz let's diff

--- mainWrapPlotLog.m
+++ mainWrapPlotLog.m
@@ -1,4 +1,4 @@
-% function mainWrapPlotLog(fileName)
+% function mainWrapPlotLog(fileName) 
 % fileName = 'datas2019-01-08_21_52.log'; %RRTconnect 1param, 2runs, 50random queries, 0:2:2s of countdown
 % fileName = 'datas2019-01-09_21_58.log'; %TRRT 9params, 2runs, 50random queries, 2:2:4s of countdown
 fileName = 'datas2019-01-09_23_15.log'; %TRRT 9params, 2runs, 50random queries, 0:2:2s of countdown
