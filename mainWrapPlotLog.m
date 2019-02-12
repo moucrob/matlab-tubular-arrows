d61bf5e85fdd01f156f4631402bee94298dcbe42
moucrob <mathiasmouchrik@gmail.com>
2019-02-12 14:53:10 +0000

    want to add the joint angle limits in the legends

--- mainWrapPlotLog.m
+++ mainWrapPlotLog.m
@@ -3,14 +3,14 @@ clc
 % fileName = 'datas2019-01-08_21_52.log'; %RRTconnect 1param, 2runs, 50random queries, 0:2:2s of countdown
 % fileName = 'datas2019-01-09_21_58.log'; %TRRT 9params, 2runs, 50random queries, 2:2:4s of countdown
 
-% fileName = 'datas2019-01-09_23_15.log'; %TRRT 9params, 2runs, 50random queries, 0:2:2s of countdown
+fileName = 'datas2019-01-09_23_15.log'; %TRRT 9params, 2runs, 50random queries, 0:2:2s of countdown
 
 % fileName = 'goodmins.log';
 % fileName = 'TRRTwrapped.log'; %not so much in that one...
 % fileName = 'ESTwrapped.log'; %skipped to see the plot from 70 to 171
 % fileName = 'RRTwrapped.log'; %i=570 to end gives good plots
 
-fileName = 'RRTCwrapped.log';
+% fileName = 'RRTCwrapped.log';
 %idx = 640 +figNumber (if initially no fig)-1
 %good figs are thus :
 %662 : shows that final parameter set is red, while optimalest one since
@@ -29,9 +29,9 @@ fileName = 'RRTCwrapped.log';
 c = loadlog(fileName);
 fig = cell(0,1) ; rules = cell(0,1);
 % for i=1:numel(c)
-for i=640:numel(c) 
-    
-% for i=24:24 %for TRRT datas2019-01-09_23_15.log
+
+% for i=[648,650,662] %for RRTC 1D
+for i=24:24 %for TRRT 9D datas2019-01-09_23_15.log
 
     [fig{end+1,1},rules{end+1,1}] = plotOptimizer(c{i});
     disp(['i = ',num2str(i)]) ; pause(4);
