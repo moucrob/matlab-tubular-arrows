34f0ba4b35db8f522879a8a686b9ea65040b3c19
moucrob <me@me.com>
2019-02-28 17:28:46 +0000

    added restricted logs

--- mainWrapPlotLog.m
+++ mainWrapPlotLog.m
@@ -1,17 +1,19 @@
 % function mainWrapPlotLog(fileName)
 clc
 clear all
-% fileName = 'datas2019-01-08_21_52.log'; %RRTconnect 1param, 2runs, 50random queries, 0:2:2s of countdown
-% fileName = 'datas2019-01-09_21_58.log'; %TRRT 9params, 2runs, 50random queries, 2:2:4s of countdown
+% fileName = 'logs/unrestricted/datas2019-01-08_21_52.log'; %RRTconnect 1param, 2runs, 50random queries, 0:2:2s of countdown
+% fileName = 'logs/unrestricted/datas2019-01-09_21_58.log'; %TRRT 9params, 2runs, 50random queries, 2:2:4s of countdown
 
-fileName = 'datas2019-01-09_23_15.log'; %TRRT 9params, 2runs, 50random queries, 0:2:2s of countdown
+%fileName = 'logs/unrestricted/datas2019-01-09_23_15.log'; %TRRT 9params, 2runs, 50random queries, 0:2:2s of countdown
 
-% fileName = 'goodmins.log';
-% fileName = 'TRRTwrapped.log'; %not so much in that one...
-% fileName = 'ESTwrapped.log'; %skipped to see the plot from 70 to 171
-% fileName = 'RRTwrapped.log'; %i=570 to end gives good plots
+fileName = 'logs/restricted/TRRTwrapped.log';
 
-% fileName = 'RRTCwrapped.log';
+% fileName = 'logs/unrestricted/goodmins.log';
+% fileName = 'logs/unrestricted/TRRTwrapped.log'; %not so much in that one...
+% fileName = 'logs/unrestricted/ESTwrapped.log'; %skipped to see the plot from 70 to 171
+% fileName = 'logs/unrestricted/RRTwrapped.log'; %i=570 to end gives good plots
+
+% fileName = 'logs/unrestricted/RRTCwrapped.log';
 %idx = 640 +figNumber (if initially no fig)-1
 %good figs are thus :
 %662 : shows that final parameter set is red, while optimalest one since
@@ -31,8 +33,9 @@ c = loadlog(fileName);
 fig = cell(0,1) ; rules = cell(0,1);
 
 % for i=1:numel(c)
+for i=251:500
 % for i=[648,650,662] %for RRTC 1D
-for i=24:24 %for TRRT 9D datas2019-01-09_23_15.log
+%for i=24:24 %for TRRT 9D datas2019-01-09_23_15.log
     [fig{end+1,1},rules{end+1,1}] = plotOptimizer(c{i});
     disp(['i = ',num2str(i)]) ; pause(4);
 end
